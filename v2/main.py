4422ed512e1d387e50ea1d778f3867cff2934936
James Jarvis <git@jamesjarvis.io>
2022-10-09 23:05:48 +0100

    this should hopefully update the xkcd stuff every 12 hours I guess

--- v2/main.py
+++ v2/main.py
@@ -12,6 +12,8 @@
 # 3 [Any]:          Photo Mode with 3 second delay, takes a pic and displays.
 
 import signal
+import datetime
+import time
 from inkydev import PIN_INTERRUPT
 import RPi.GPIO as GPIO
 
@@ -23,6 +25,10 @@ GPIO.setup(PIN_INTERRUPT, GPIO.IN, pull_up_down=GPIO.PUD_UP)
 # Set up the display logic.
 display = Display(inky_display)
 
+XKCD_MODE_ACTIVE = True
+XKCD_MODE_LAST_UPDATED = None
+XKCD_MODE_INTERVAL_SECONDS = 60 * 60 * 12 # 12 hours.
+
 def handle_interrupt(pin):
     button_a, button_b, button_c, button_d, changed = inkydev.read_buttons()
 
@@ -30,15 +36,30 @@ def handle_interrupt(pin):
         return
 
     if button_a:
-        return
-    if button_b or button_c:
+        XKCD_MODE_ACTIVE = True
+        display.xkcd()
+        XKCD_MODE_LAST_UPDATED = datetime.utcnow()
+    elif button_b or button_c:
+        XKCD_MODE_ACTIVE = False
         display.take_picture(0)
-        return
-    if button_d:
+    elif button_d:
+        XKCD_MODE_ACTIVE = False
         display.take_picture(4)
-        return
+
+    display.setup_leds()
+    return
 
 
 GPIO.add_event_detect(PIN_INTERRUPT, GPIO.FALLING, callback=handle_interrupt)
+
+while True:
+    time.sleep(1)
+    if not XKCD_MODE_ACTIVE:
+        continue
+    current_time = datetime.utcnow()
+    if not XKCD_MODE_LAST_UPDATED or (current_time - XKCD_MODE_LAST_UPDATED).total_seconds() > XKCD_MODE_INTERVAL_SECONDS:
+        display.xkcd()
+        XKCD_MODE_LAST_UPDATED = datetime.utcnow()
+        continue
 
 signal.pause()
