d3e1e90a0af0964dacc45f0a012467079a3df1d5
James Jarvis <git@jamesjarvis.io>
2024-08-25 18:13:59 +0100

    format display.py

--- v2/display.py
+++ v2/display.py
@@ -1,4 +1,3 @@
-
 import inky.inky_uc8159 as inky
 from inkydev import InkyDev
 from time import sleep
@@ -9,112 +8,115 @@ import api
 SIZE_X, SIZE_Y = 600, 448
 SATURATION = 0.5
 
-class Display():
-  """
-  Display contains all logic for driving the InkyDev display.
-  Fundamentally we have a "background" and a "overlay" component to the UI.
-  The background is an image, that is set to the total size of the display, and the overlay
-  is a section of the display roughly 1/3rd the height of the display, from the bottom.
-  It will contain:
-  - Weather (bottom left)
-  - Train times + important days (bottom right)
-  """
-  def __init__(self):
-    self.inky_display = inky.Inky((SIZE_X, SIZE_Y))
-    self.inky_dev = InkyDev()
-    
-    self.setup_leds()
 
-  def setup_leds(self):
+class Display:
     """
-    setup_leds will set all LEDs to their default colours.
+    Display contains all logic for driving the InkyDev display.
+    Fundamentally we have a "background" and a "overlay" component to the UI.
+    The background is an image, that is set to the total size of the display, and the overlay
+    is a section of the display roughly 1/3rd the height of the display, from the bottom.
+    It will contain:
+    - Weather (bottom left)
+    - Train times + important days (bottom right)
+
+    The display is in portrait mode.
     """
-    # Initialise LEDs for their program function
-    # 0: XKCD   (white)
-    # 1: Photo  (blue)
-    # 2: Photo  (blue)
-    # 3: Photo with 3 sec delay (green)
-    self.inky_dev.set_led(0, 5, 5, 5)
-    self.inky_dev.set_led(1, 0, 0, 5)
-    self.inky_dev.set_led(2, 0, 0, 5)
-    self.inky_dev.set_led(3, 0, 5, 0)
-    self.inky_dev.update()
 
-  def set_all_leds(self, r, g, b, update=True):
-    """
-    set_all_leds sets all LED's to the given colour.
-    """
-    self.inky_dev.set_led(0, r, g, b)
-    self.inky_dev.set_led(1, r, g, b)
-    self.inky_dev.set_led(2, r, g, b)
-    self.inky_dev.set_led(3, r, g, b)
-    if update:
-      self.inky_dev.update()
+    def __init__(self):
+        self.inky_display = inky.Inky((SIZE_X, SIZE_Y))
+        self.inky_dev = InkyDev()
 
-  def take_picture(self, delay:int):
-    """
-    take_picture takes a picture with the onboard camera,
-    after the given delay (in seconds).
-    Then it will update the onboard display with the picture.
-    """
-    print("Taking a picture...")
+        self.setup_leds()
 
-    # Start the countdown
-    for i in range(delay, 0, -1):
-      self.set_all_leds(0, 0, 0, update=False)
-      if i > 0:
-        self.inky_dev.set_led(0, 255, 255, 255)
-      if i > 1:
-        self.inky_dev.set_led(1, 255, 255, 255)
-      if i > 2:
-        self.inky_dev.set_led(2, 255, 255, 255)
-      if i > 3:
-        self.inky_dev.set_led(3, 255, 255, 255)
-      self.inky_dev.update()
-      sleep(1)
+    def setup_leds(self):
+        """
+        setup_leds will set all LEDs to their default colours.
+        """
+        # Initialise LEDs for their program function
+        # 0: XKCD   (white)
+        # 1: Photo  (blue)
+        # 2: Photo  (blue)
+        # 3: Photo with 3 sec delay (green)
+        self.inky_dev.set_led(0, 5, 5, 5)
+        self.inky_dev.set_led(1, 0, 0, 5)
+        self.inky_dev.set_led(2, 0, 0, 5)
+        self.inky_dev.set_led(3, 0, 5, 0)
+        self.inky_dev.update()
 
-    # Now we want to try and take a pic? I guess...
+    def set_all_leds(self, r, g, b, update=True):
+        """
+        set_all_leds sets all LED's to the given colour.
+        """
+        self.inky_dev.set_led(0, r, g, b)
+        self.inky_dev.set_led(1, r, g, b)
+        self.inky_dev.set_led(2, r, g, b)
+        self.inky_dev.set_led(3, r, g, b)
+        if update:
+            self.inky_dev.update()
 
-    self.set_all_leds(255, 255, 255)
+    def take_picture(self, delay: int):
+        """
+        take_picture takes a picture with the onboard camera,
+        after the given delay (in seconds).
+        Then it will update the onboard display with the picture.
+        """
+        print("Taking a picture...")
 
-    try:
-      image = camera.take_picture()
+        # Start the countdown
+        for i in range(delay, 0, -1):
+            self.set_all_leds(0, 0, 0, update=False)
+            if i > 0:
+                self.inky_dev.set_led(0, 255, 255, 255)
+            if i > 1:
+                self.inky_dev.set_led(1, 255, 255, 255)
+            if i > 2:
+                self.inky_dev.set_led(2, 255, 255, 255)
+            if i > 3:
+                self.inky_dev.set_led(3, 255, 255, 255)
+            self.inky_dev.update()
+            sleep(1)
 
-      self.set_all_leds(0, 0, 50)
+        # Now we want to try and take a pic? I guess...
 
-      print("Picture taken, displaying...")
+        self.set_all_leds(255, 255, 255)
 
-      image = image.resize(self.inky_display.resolution)
-      self.inky_display.set_image(image, saturation=SATURATION)
-      self.inky_display.show()
-    except Exception as e:
-      print("Failed to set the image...")
-      print(e)
-      self.set_all_leds(255, 0, 0)
-      return
+        try:
+            image = camera.take_picture()
 
-    print("Picture displayed...")
+            self.set_all_leds(0, 0, 50)
 
-  def xkcd(self):
-    """
-    xkcd will display a random XKCD comic every day.
-    """
-    print("Fetching XKCD...")
-    
-    try:
-      image = api.xkcd()
+            print("Picture taken, displaying...")
 
-      self.set_all_leds(0, 0, 50)
+            image = image.resize(self.inky_display.resolution)
+            self.inky_display.set_image(image, saturation=SATURATION)
+            self.inky_display.show()
+        except Exception as e:
+            print("Failed to set the image...")
+            print(e)
+            self.set_all_leds(255, 0, 0)
+            return
 
-      print("Comic fetched, displaying...")
+        print("Picture displayed...")
 
-      self.inky_display.set_image(image)
-      self.inky_display.show()
-    except Exception as e:
-      print("Failed to set the image...")
-      print(e)
-      self.set_all_leds(255, 0, 0)
-      return
+    def xkcd(self):
+        """
+        xkcd will display a random XKCD comic every day.
+        """
+        print("Fetching XKCD...")
 
-    print("Comic displayed...")
-  
+        try:
+            image = api.xkcd()
+
+            self.set_all_leds(0, 0, 50)
+
+            print("Comic fetched, displaying...")
+
+            self.inky_display.set_image(image)
+            self.inky_display.show()
+        except Exception as e:
+            print("Failed to set the image...")
+            print(e)
+            self.set_all_leds(255, 0, 0)
+            return
+
+        print("Comic displayed...")
