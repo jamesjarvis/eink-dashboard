bf64873095dac0f8592d14cc867f0aa598d5e184
James Jarvis <git@jamesjarvis.io>
2024-08-25 22:15:51 +0100

    add some test files

--- v2/graphics.py
+++ v2/graphics.py
@@ -1,6 +1,19 @@
-from PIL import Image
+from PIL import Image, ImageDraw
+from datatypes import WeatherData, TrainData
 
-def draw_overlay(image: Image.Image):
+SIZE_X, SIZE_Y = 448, 600
+
+def draw_overlay(
+  image: Image.Image,
+  weather: WeatherData,
+  train: TrainData,
+):
+  draw = ImageDraw.Draw(image)
+
+  # Consistent weather box 298 x 120 px
+  draw.rectangle(
+    (0, SIZE_Y, 280, SIZE_Y-120), fill=(255,255,255)
+  )
 
 
   image.show()
--- v2/test.py
+++ v2/test.py
@@ -1,9 +1,33 @@
 #!/usr/bin/env python3
 from storage import Storage
+import api
 import graphics
 
 storage = Storage()
 
+# Update weather data
+settings = storage.get_settings()
+weather_data = api.get_forecast(
+    lat=settings["latitude"],
+    lon=settings["longitude"],
+    api_key=settings["tomorrow_api_key"],
+)
+weather_data.sunrise, weather_data.sunset = api.get_sunrise_and_sunset(
+    lat=settings["latitude"],
+    lon=settings["longitude"],
+)
+storage.set_weather_data(weather_data)
+
+# Update train data
+train_data = api.get_train_departure_times(
+    username=settings["realtime_trains_username"],
+    password=settings["realtime_trains_password"],
+    station_code=settings["train_station"],
+)
+storage.set_train_data(train_data)
+
 graphics.draw_overlay(
   image=storage.get_latest_image(),
+  weather=storage.get_weather_data(),
+  train=storage.get_train_data(),
 )
