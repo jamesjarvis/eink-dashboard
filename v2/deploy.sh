9631f0367408ed1e173d373feded6143d8cbefec
James Jarvis <git@jamesjarvis.io>
2024-08-25 19:10:43 +0100

    update deploy script

--- v2/deploy.sh
+++ v2/deploy.sh
@@ -7,7 +7,10 @@ HOST_LOCATION=/home/pi/
 
 scp api.py $HOST:$HOST_LOCATION
 scp camera.py $HOST:$HOST_LOCATION
+scp datatypes.py $HOST:$HOST_LOCATION
 scp display.py $HOST:$HOST_LOCATION
 scp main.py $HOST:$HOST_LOCATION
+scp storage.py $HOST:$HOST_LOCATION
+scp settings.json $HOST:$HOST_LOCATION
 
 echo "******************** Deployed **********************"
