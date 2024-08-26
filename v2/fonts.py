5ac1f2d6589dd50bc138d375c2b0bf0dc6b67274
James Jarvis <git@jamesjarvis.io>
2024-08-26 05:59:28 +0100

    add fonts package

---
+++ v2/fonts.py
@@ -0,0 +1,12 @@
+# Fonts
+FONT_OPEN_SANS = "fonts/OpenSans-SemiBold.ttf"
+FONT_ROBOTO_MONO = "fonts/RobotoMono-Medium.ttf"
+FONT_WEATHER = "fonts/weatherfont.ttf"
+
+
+# Icons, taken straight from https://www.weatherfont.com/
+ICON_SUNRISE = "\uf051"
+ICON_SUNSET = "\uf052"
+ICON_CELSIUS = "\uf03c"
+ICON_THERMOMETER = "\uf053"
+ICON_UMBRELLA = "\uf084"
