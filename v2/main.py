701344b9d2ee0dbaf519df7b8667e4c06360e78f
James Jarvis <git@jamesjarvis.io>
2024-08-26 16:47:26 +0100

    add a massive try catch around the data retrieval

--- v2/main.py
+++ v2/main.py
@@ -101,26 +101,33 @@ while True:
     ):
         continue
 
-    # Update weather data
-    settings = storage.get_settings()
-    weather_data = api.get_forecast(
-        lat=settings["latitude"],
-        lon=settings["longitude"],
-        api_key=settings["tomorrow_api_key"],
-    )
-    weather_data.sunrise, weather_data.sunset = api.get_sunrise_and_sunset(
-        lat=settings["latitude"],
-        lon=settings["longitude"],
-    )
-    storage.set_weather_data(weather_data)
+    logging.debug("Updating data from external sources")
 
-    # Update train data
-    train_data = api.get_train_departure_times(
-        username=settings["realtime_trains_username"],
-        password=settings["realtime_trains_password"],
-        station_code=settings["train_station"],
-    )
-    storage.set_train_data(train_data)
+    try:
+        logging.debug("Updating weather data")
+        # Update weather data
+        settings = storage.get_settings()
+        weather_data = api.get_forecast(
+            lat=settings["latitude"],
+            lon=settings["longitude"],
+            api_key=settings["tomorrow_api_key"],
+        )
+        weather_data.sunrise, weather_data.sunset = api.get_sunrise_and_sunset(
+            lat=settings["latitude"],
+            lon=settings["longitude"],
+        )
+        storage.set_weather_data(weather_data)
+
+        logging.debug("Updating train data")
+        # Update train data
+        train_data = api.get_train_departure_times(
+            username=settings["realtime_trains_username"],
+            password=settings["realtime_trains_password"],
+            station_code=settings["train_station"],
+        )
+        storage.set_train_data(train_data)
+    except Exception as e:
+        logging.error("error encountered while updating data", exc_info=e)
 
     # Redraw display
     display.redraw()
